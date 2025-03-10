34c216ed1493b7f4ec4547c16e5397d6ab5787fa
下館諒 <shimodatemakoto@shimodateryounoMacBook-Air.local>
2025-03-10 12:05:06 +0900

    ストップウォッチ提出用

---
+++ main.js
@@ -0,0 +1,56 @@
+// ストップウォッチの定義
+const $watch = document.getElementById("js-stopwatch");
+const $start = document.getElementById("js-stopwatchStart");
+const $stop = document.getElementById("js-stopwatchStop");
+const $resset = document.getElementById("js-stopwatchResset");
+
+// 開始時間
+let startTime;
+
+// 停止時間
+let stopTime = 0;
+
+// タイムアウトID
+let timeoutID;
+
+$watch.textContent ='0 : 0 : 0 : 0:'
+
+// 時間を表示する関数
+function displayTime() {
+  
+  const currentTime = new Date(Date.now() - startTime + stopTime);
+  const h = String(currentTime.getHours() - 9);
+  const m = String(currentTime.getMinutes());
+  const s = String(currentTime.getSeconds());
+  const ms = String(currentTime.getMilliseconds() % 10  );
+  $watch.textContent = `${h}:${m}:${s}.${ms}`;
+  timeoutID = setTimeout(displayTime, 10);
+}
+
+// スタートボタンがクリックされたら時間を進める部分
+$start.addEventListener("click", () => {
+  $start.disabled = true;
+  $stop.disabled = false;
+  $resset.disabled = true;
+  startTime = Date.now();
+  displayTime();
+  $watch.textContent = "0:0:0:0";
+});
+
+// ストップボタンがクリックされたら時間を止める部分
+$stop.addEventListener("click", function () {
+  $start.disabled = false;
+  $stop.disabled = true;
+  $resset.disabled = false;
+  clearTimeout(timeoutID);
+  stopTime += Date.now() - startTime;
+});
+
+// リセットボタンがクリックされたら時間を0に戻す部分
+$resset.addEventListener("click", function () {
+  $start.disabled = false;
+  $stop.disabled = true;
+  $resset.disabled = true;
+  $watch.textContent = "0:0:0:0";
+  stopTime = 0;
+});
